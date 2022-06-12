bfc5c1609486b8281a3b725cdded767bd678c034
Miguel Rios R <mgl.rios.r@gmail.com>
2022-06-11 23:25:23 -0500

    Initial Commit

---
+++ GameViewApp/GameViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameViewAppApp.swift
+//  GameViewApp
+//
+//  Created by Miguel Rios on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct GameViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
